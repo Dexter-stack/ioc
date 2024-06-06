2600ab5226264f2caae3b0c0727093f8de693010
dexter-stack <muhtohasulaimon@gmail.com>
2024-06-06 07:47:05 -0700

    application.yml file added

--- ioc/src/main/java/com/dexter/ioc/controller/HelloController.java
+++ ioc/src/main/java/com/dexter/ioc/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.dexter.ioc.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
 //    @RequestMapping(value = "/", method = RequestMethod.GET)
+
     //or
+
+    @Value("${welcome.message}")
+    private String WelcomeMessage;
     @GetMapping("/")
     public String helloworld(){
-        return "Welcome  to Dexter aaa ddd dexet";
+        return WelcomeMessage;
 
     }
 
